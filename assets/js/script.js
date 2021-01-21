96929815a5af923fead24fee285fd57b3d2e2d81
Xander Cage <cagex491@gmail.com>
2021-01-21 19:03:18 +0300

    Modified about.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-function saveImage(imgSrc){
+function saveImage(imgSrc) {
     sessionStorage.setItem('imgSrc', imgSrc)
-    location.href='buy.html';
+    location.href = 'buy.html';
 }
 
 // console.log(document.querySelector("div.container.my-5 > div > div:nth-child(1) > div > img").src)
 
-document.getElementById("buyBtn").addEventListener("click", function(){
+document.getElementById("buyBtn").addEventListener("click", function () {
     window.location.href = "buy.html"
-    
+
 })
 
 // if(window.location.pathname === '/buy.html'){
